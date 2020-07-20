88f8fe5f69f60089ec385daaf5aa522bf79d068a
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-07-20 23:52:01 +0100

    Feature: Written init method for matrix

---
+++ matrix/matrix.py
@@ -0,0 +1,16 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        matrix_list = matrix_string.split('\n')[1:-1]
+        self.matrix = [[int(j) for j in i.split()] for i in matrix_list]
+
+    def row(self, index):
+        pass
+
+    def column(self, index):
+        pass
+
+Matrix('''
+1 2 3
+2 3 4
+3 4 5
+''')
